d1d81ef3c783ef60565abdbecbc8f846e25c9a1e
Guillaume de Sagazan <yazgazan@gmail.com>
2017-08-03 20:16:18 +0200

    fixing reader's tests

--- reader/tests.sh
+++ reader/tests.sh
@@ -1,9 +1,17 @@
 #!/usr/bin/env bash
 
 V=$(echo "hello world" | PS1="" ./demo/reader-demo)
-if [[ "$V" != $'read command line: hello world\nexiting...' ]]; then
+
+echo $V | grep 'read command line: hello world' > /dev/null
+if [[ $? != 0 ]]; then
 	echo 'FAILED `echo "hello world" | ./demo/reader-demo`'
-	echo "'$V'"
+	echo "should contain 'read command line: hello world'"
 	exit 1
 fi
 
+echo $V | grep 'exiting...' > /dev/null
+if [[ $? != 0 ]]; then
+	echo 'FAILED `echo "hello world" | ./demo/reader-demo`'
+	echo "should contain 'exiting...'"
+	exit 1
+fi
