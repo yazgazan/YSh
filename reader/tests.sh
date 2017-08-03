644d951d21da65e7ef927b99dc2eec5e9fed0dc8
Guillaume de Sagazan <yazgazan@gmail.com>
2017-08-03 19:36:45 +0200

    Trying to fix travis build

--- reader/tests.sh
+++ reader/tests.sh
@@ -1,10 +1,9 @@
 #!/usr/bin/env bash
 
-make -C demo || exit $?
-
 V=$(echo "hello world" | ./demo/reader-demo)
 if [[ "$V" != $'read command line: hello world\nexiting...' ]]; then
 	echo 'FAILED `echo "hello world" | ./demo/reader-demo`'
+	echo "'$V'"
 	exit 1
 fi
 
